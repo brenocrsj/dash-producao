e9032789fb27998184e7256bfc9468b8b3d19ea6
brenocrsj <brenocarlos200601@gmail.com>
2025-06-13 17:29:50 -0300

    Adiciona novas funcionalidades e corrige bugs

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 # run.py
-from app import create_app
 
-app, server = create_app()
+# Importa as instâncias 'app' e 'server' que são definidas globalmente em app.py
+from app import app, server 
 
 if __name__ == '__main__':
-    server.run(debug=True, port=8080)
+    # Aqui você pode rodar o servidor Dash para desenvolvimento
+    # A variável 'app' é a instância do Dash, e 'server' é a instância do Flask
+    # CORRIGIDO: app.run_server() foi substituído por app.run()
+    app.run(debug=True) # Use app.run() em vez de app.run_server()
